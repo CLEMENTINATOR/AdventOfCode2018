b3a0a269bce8efc7d3f8b55a838be3e043ea0f83
Clément B <bouvetcl@gmail.com>
2018-12-15 09:40:04 +0100

    01122018.py: add part2

--- 01122018/01122018.py
+++ 01122018/01122018.py
@@ -8,11 +8,25 @@ def part1(freq_changes):
 
     print("End freq : {}".format(cur_freq))
 
+def part2(freq_changes):
+    freqs_already_reached = [0]
+    cur_freq = 0
+
+    while True:
+        for freq_change in freq_changes:
+            cur_freq += int(freq_change)
+            if cur_freq in freqs_already_reached:
+                print("{} reached twice".format(cur_freq))
+                return
+            else:
+                freqs_already_reached.append(cur_freq)
+
 
 def main():
     with open("input", "r") as f:
         freq_changes = f.read().split("\n")
     part1(freq_changes)
+    part2(freq_changes)
     
 if __name__ == "__main__":
     main()
